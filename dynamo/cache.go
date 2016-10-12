0f72841fdc30db8f880affe9e634cbd32b6e0ac5
ftuyama <ftuyama@gmail.com>
2016-10-12 01:43:33 -0300

    Indentação2

--- dynamo/cache.go
+++ dynamo/cache.go
@@ -1,8 +1,8 @@
 package dynamo
 
 import (
-	"log"
-	"sync"
+    "log"
+    "sync"
 )
 
 /*
@@ -11,50 +11,50 @@ import (
 
 // Cache is the struct that handle all the data storage for the dynamo server.
 type Cache struct {
-	data      map[string]string
-	timestamp map[string]int64
-	sync.Mutex
+    data      map[string]string
+    timestamp map[string]int64
+    sync.Mutex
 }
 
 // Create a new cache object and return a pointer to it.
 func NewCache() *Cache {
-	var s Cache
+    var s Cache
 
-	s.data = make(map[string]string)
-	s.timestamp = make(map[string]int64)
+    s.data = make(map[string]string)
+    s.timestamp = make(map[string]int64)
 
-	return &s
+    return &s
 }
 
 // Get the value of a key from the storage. This will handle concurrent get
 // requests by locking the structure.
 func (cache *Cache) Get(key string) (value string, timestamp int64) {
-	cache.Lock()
-	value = cache.data[key]
-	timestamp = cache.timestamp[key]
-	cache.Unlock()
+    cache.Lock()
+    value = cache.data[key]
+    timestamp = cache.timestamp[key]
+    cache.Unlock()
 
-	log.Printf("[CACHE] Getting Key '%v' with Value '%v' @ timestamp '%v'\n", key, value, timestamp)
-	return value, timestamp
+    log.Printf("[CACHE] Getting Key '%v' with Value '%v' @ timestamp '%v'\n", key, value, timestamp)
+    return value, timestamp
 }
 
 // Put a value to a key in the storage. This will handle concurrent put
 // requests by locking the structure.
 func (cache *Cache) Put(key string, value string, timestamp int64) {
-	log.Printf("[CACHE] Putting Key '%v' with Value '%v' @ timestamp '%v'\n", key, value, timestamp)
+    log.Printf("[CACHE] Putting Key '%v' with Value '%v' @ timestamp '%v'\n", key, value, timestamp)
 
-	cache.Lock()
-	cache.data[key] = value
-	cache.timestamp[key] = timestamp
-	cache.Unlock()
+    cache.Lock()
+    cache.data[key] = value
+    cache.timestamp[key] = timestamp
+    cache.Unlock()
 
-	return
+    return
 }
 
 // Retrieve all information from the server. This shouldn't be used in any way
 // except for testing purposes.
 func (cache *Cache) getAll() (data map[string]string, timestamps map[string]int64) {
-	data = cache.data
-	timestamps = cache.timestamp
-	return data, timestamps
+    data = cache.data
+    timestamps = cache.timestamp
+    return data, timestamps
 }
